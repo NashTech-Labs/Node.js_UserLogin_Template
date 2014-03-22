2429892d6f21bb35c723a5c076bfdd547b491138
ayushmishra2005 <ayushmishra2005@gmail.com>
2014-03-22 07:58:16 +0530

    SignUp using Social Sites

--- app/routes.js
+++ app/routes.js
@@ -34,8 +34,67 @@ module.exports = function(app, passport) {
 		}));
 
 
-};
 
+// GET /auth/facebook
+// Use passport.authenticate() as route middleware to authenticate the
+// request. The first step in Facebook authentication will involve
+// redirecting the user to facebook.com. After authorization, Facebook will
+// redirect the user back to this application at /auth/facebook/callback
+		app.get('/auth/facebook',
+  			passport.authenticate('facebook',{ scope : 'email' }));
+
+// GET /auth/facebook/callback
+// Use passport.authenticate() as route middleware to authenticate the
+// request. If authentication fails, the user will be redirected back to the
+// login page. Otherwise, the primary route function function will be called,
+// which, in this example, will redirect the user to the home page.
+		app.get('/auth/facebook/callback',
+  			passport.authenticate('facebook', { 
+				successRedirect : '/user', 	
+				failureRedirect: '/login' }));
+
+
+
+
+
+// GET /auth/twitter
+// Use passport.authenticate() as route middleware to authenticate the
+// request. The first step in Twitter authentication will involve redirecting
+// the user to twitter.com. After authorization, the Twitter will redirect
+// the user back to this application at /auth/twitter/callback
+app.get('/auth/twitter',
+  passport.authenticate('twitter'));
+
+// GET /auth/twitter/callback
+// Use passport.authenticate() as route middleware to authenticate the
+// request. If authentication fails, the user will be redirected back to the
+// login page. Otherwise, the primary route function function will be called,
+// which, in this example, will redirect the user to the home page.
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { 
+				successRedirect : '/user', 	
+				failureRedirect: '/login' }));
+
+
+// GET /auth/google
+// Use passport.authenticate() as route middleware to authenticate the
+// request. The first step in Google authentication will involve
+// redirecting the user to google.com. After authorization, Google
+// will redirect the user back to this application at /auth/google/callback
+app.get('/auth/google',
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+// GET /auth/google/callback
+// Use passport.authenticate() as route middleware to authenticate the
+// request. If authentication fails, the user will be redirected back to the
+// login page. Otherwise, the primary route function function will be called,
+// which, in this example, will redirect the user to the home page.
+app.get('/auth/google/callback',
+  passport.authenticate('google', { 
+				successRedirect : '/user', 	
+				failureRedirect: '/login' }));
+
+};
 function auth(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,25 @@
 // local authentication
 // For more details go to https://github.com/jaredhanson/passport-local
 var LocalStrategy    = require('passport-local').Strategy;
+
+// Facebook authentication
+// For more details go to https://github.com/jaredhanson/passport-facebook
+var FacebookStrategy = require('passport-facebook').Strategy;
+var FACEBOOK_APP_ID = "<Insert Your Key Here>"
+var FACEBOOK_APP_SECRET = "<Insert Your Secret Key Here>";
+
+// Twitter authentication
+// For more details go to https://github.com/jaredhanson/passport-twitter
+var TwitterStrategy = require('passport-twitter').Strategy;
+var TWITTER_CONSUMER_KEY = "<Insert Your Key Here>";
+var TWITTER_CONSUMER_SECRET = "<Insert Your Secret Key Here>";
+
+// Google authentication
+// For more details go to https://github.com/jaredhanson/passport-google-oauth
+var GOOGLE_CONSUMER_KEY = "<Insert Your Key Here>";
+var GOOGLE_CONSUMER_SECRET = "<Insert Your Secret Key Here>";
+var GoogleStrategy   = require('passport-google-oauth').OAuth2Strategy
+
 var User       = require('../app/models/user');
 
 module.exports = function(passport) {
@@ -81,5 +100,137 @@ module.exports = function(passport) {
 
 
     }));
+
+// Use the FacebookStrategy within Passport.
+// Strategies in Passport require a `verify` function, which accept
+// credentials (in this case, an accessToken, refreshToken, and Facebook
+// profile), and invoke a callback with a user object.
+	passport.use(new FacebookStrategy({
+    		clientID: FACEBOOK_APP_ID,
+    		clientSecret: FACEBOOK_APP_SECRET,
+    		callbackURL: "http://localhost:8080/auth/facebook/callback"
+  		},
+  		function(req, accessToken, refreshToken, profile, done) {
+    		// asynchronous verification, for effect...
+
+    			process.nextTick(function () {
+            			if (!req.user) {
+ 					User.findOne({ 'user.email' :  profile.emails[0].value }, function(err, user) {
+            	    				if (err){ return done(err);}
+                    				if (user) {
+                        				return done(null, user);
+                    				} else {
+                        				var newUser            = new User();
+							newUser.user.username    = profile.displayName;
+                        				newUser.user.email    = profile.emails[0].value;
+                        				newUser.save(function(err) {
+                            					if (err)
+                                					throw err;
+                            				return done(null, newUser);
+                        				});
+                    				}
+
+                			});
+                         	} else {
+					var user            = req.user;
+					user.user.username    = profile.displayName;
+                			user.user.email    = profile.emails[0].value;
+                			user.save(function(err) {
+                    				if (err)
+                        				throw err;
+                    			return done(null, user);
+                			});
+            			}
+    			});
+  		}
+	));
+
+// Use the TwitterStrategy within Passport.
+// Strategies in passport require a `verify` function, which accept
+// credentials (in this case, a token, tokenSecret, and Twitter profile), and
+// invoke a callback with a user object.
+			passport.use(new TwitterStrategy({
+    			consumerKey: TWITTER_CONSUMER_KEY,
+    			consumerSecret: TWITTER_CONSUMER_SECRET,
+    			callbackURL: "http://192.168.1.101:8080/auth/twitter/callback"
+  		},
+  		function(req,token, tokenSecret, profile, done) {
+    // asynchronous verification, for effect...
+    			process.nextTick(function () {
+      
+     				 if (!req.user) {
+ 					User.findOne({ 'user.username' :  profile.displayName }, function(err, user) {
+            	    				if (err){ return done(err);}
+                    				if (user) {
+                        				return done(null, user);
+                    				} else {
+                        				var newUser            = new User();
+							newUser.user.username    = profile.displayName;
+                        				newUser.save(function(err) {
+                            					if (err)
+                                					throw err;
+                            				return done(null, newUser);
+                        				});
+                    				}
+
+                			});
+                         	} else {
+					var user            = req.user;
+					user.user.username    = profile.displayName;
+                			user.save(function(err) {
+                    				if (err)
+                        				throw err;
+                    			return done(null, user);
+                			});
+            			}
+    			});
+  		}		
+	));
+
+// Use the GoogleStrategy within Passport.
+// Strategies in Passport require a `verify` function, which accept
+// credentials (in this case, an accessToken, refreshToken, and Google
+// profile), and invoke a callback with a user object.
+		passport.use(new GoogleStrategy({
+    				clientID: GOOGLE_CONSUMER_KEY,
+    				clientSecret: GOOGLE_CONSUMER_SECRET,
+    				callbackURL: "http://localhost:8080/auth/google/callback"
+  				},
+  				function(req, accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    					process.nextTick(function () {
+      
+     						if (!req.user) {
+ 							User.findOne({ 'user.email' :  profile.emails[0].value }, function(err, user) {
+            	    						if (err){ return done(err);}
+                    					if (user) {
+                        					return done(null, user);
+                    					} else {
+                        					var newUser            = new User();
+								newUser.user.username    = profile.displayName;
+								newUser.user.email    = profile.emails[0].value;
+                        					newUser.save(function(err) {
+                            						if (err)
+                                						throw err;
+                            					return done(null, newUser);
+                        					});
+                    					}
+
+                					});
+                         			} else {
+							var user            = req.user;
+							user.user.username    = profile.displayName;
+							user.user.email    = profile.emails[0].value;
+                					user.save(function(err) {
+                    					if (err)
+                        					throw err;
+                    					return done(null, user);
+							});
+						}
+                			});
+
+    			}
+ 
+));
 
 };
